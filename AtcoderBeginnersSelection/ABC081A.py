5134f1eee390944cf5d8f19f253a43f5178d9c27
L_or_R <49993650+rikukawamura@users.noreply.github.com>
2021-01-13 14:47:45 +0900

    initial commit

---
+++ AtcoderBeginnersSelection/ABC081A.py
@@ -0,0 +1,11 @@
+x = input()
+
+count = 0
+if x[0] == "1":
+    count += 1
+if x[1] == "1":
+    count += 1
+if x[2] == "1":
+    count += 1
+
+print(count)
---
+++ AtcoderBeginnersSelection/ABC086A.py
@@ -0,0 +1,8 @@
+a, b = input().split()
+a = int(a)
+b = int(b)
+
+if a*b%2 == 0:
+    print("Even")
+else:
+    print("Odd")
